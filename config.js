f86ade5418eeec5b5980a874a57b224d35775e52
myjohorn <myjohorn@gmail.com>
2026-07-14 11:50:40 +0800

    Configure active Supabase credentials

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 // Please replace the placeholders below with your actual Supabase Project URL and Anon Key.
 // You can find these in your Supabase Dashboard under Settings > API.
 
-const SUPABASE_URL = "";
-const SUPABASE_ANON_KEY = "";
+const SUPABASE_URL = "https://wvoyxfwmshiwzuowerqj.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind2b3l4Zndtc2hpd3p1b3dlcnFqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM5ODUzMDEsImV4cCI6MjA5OTU2MTMwMX0.XLddrsOBBEG2z1i2-XZ9Tkdwg36IFMTn5TU8zhY4pZc";
